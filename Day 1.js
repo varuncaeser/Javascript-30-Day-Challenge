55f3b5e62b273365661d1f85d56a940cc9d2df9c
Varun Malyam <115411179+varuncaeser@users.noreply.github.com>
2024-07-14 23:00:49 +0530

    Add files via upload

---
+++ Day 1.js
@@ -0,0 +1,38 @@
+/* Day 1: Varibles and Data Types
+Tasks /Activities
+Activity 1 : Variale Delclaration 
+->Task 1 : Declare a variable using var , assign it a number , and log the values to the console.*/
+var num = 10
+console.log(num)
+
+
+/*->Task 2 : Declare a variable using let , assign it a number , and log the values to the console.*/
+let num2 = 20
+console.log(num2)
+/* Activity 2: Constant Declaration 
+-> Task 3: Declare a variable using const, assign it a boolean value ,and log the value to the console.
+*/
+const a=true
+console.log(a)
+/* Activity 3: Data Types 
+/*-> Task 4: Create a variable of different data types (number,string,boolean,object,array)and log each variable's type using the typeof operator*/
+var num3 = 10
+console.log(typeof(num3))
+var str = "Hello World"
+console.log(typeof str)
+var boolean=false
+console.log(typeof boolean)
+var object={name:'varun'}
+console.log(typeof object)
+const cars=["saab"]
+console.log(typeof cars)
+/* Activity 4: Reassigning Variables
+-> Task 5: Declare a variable using let,assign it an initial value, reassign a new value, and log both values to the console*/
+let num4 = 10
+num4=20
+console.log(num4)
+/* Activity 5: Understanding const
+--> Task 6: Try reassigning a varible declared with const and observe the error*/
+const num5 = 18
+num5=20
+console.log(num5)
---
+++ Day 2.js
@@ -0,0 +1,45 @@
+/* Operators
+Tasks/Activities
+Activity 1: Arithmetic Operations
+->Task 1: Write a program to add two numbers and log the result to the console*/
+const num1 = 10;
+const num2 = 20;
+console.log(num1 + num2);
+/*->Task 2: Write a program to subtract two numbers and log the result to the console*/
+console.log(num1 - num2);
+/*->Task 3: Write a program to multiply two numbers and log the result to the console*/
+console.log(num1 * num2);
+/*->Task 4: Write a program to divide two numbers and log the result to the console*/
+console.log(num1 / num2);
+/*->Task 5: Write a program to find the remainder of two numbers and log the result*/
+console.log(num1 % num2);
+/* Activity 2: Assignment Operators
+->Task 6: Use the += operator to add a number to a variable and log the result to the console*/
+let num3 = 10;
+num3 += 20;
+console.log(num3)
+/*->Task 7: Use the -= operator to subtract a number from a variable and log the console*/
+num3 -= 20;
+console.log(num3)
+/* Activity 3: Comparision Operators
+->Task 8: Write a program to compare two numbers using > and < and log the result to the console*/
+console.log(num1 > num2);
+console.log(num1 < num2);
+/*->Task 9: Write a program to compare two numbers using >= and <= and log the console*/
+console.log(num1 >= num2);
+console.log(num1 <= num2);
+/*->Task 10: Write a program to compare two numbers using == and === and log the console*/
+console.log(num1 == num2);
+console.log(num1 === num2);
+/* Activity 4:Logical Operations
+->Task 11: Write a program that uses the && operator to combine two conditions and log the result to the console*/
+console.log(num1 > num2 && num1 < num2);
+/*->Task 12: Write a program that uses the || operator to combine two conditions and log the console*/
+console.log(num1 > num2 || num1 < num2);
+/*->Task 13: Write a program that uses the ! operator to negate a condition and log console*/
+console.log(!num1 > num2);
+/* Activity 5: Terenary Operator
+->Task 14: Write a program that uses the ternary operator to check if a number is positive or negative and log the result to the console*/
+const num4 = 10;
+console.log(num4 > 0 ? "Positive" :"Negative");
+
